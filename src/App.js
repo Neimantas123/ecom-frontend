57a47315ce86b07e76b858541adbae1908393dfe
Neimantas <samochvalovas.neimantas@gmail.com>
2022-12-28 14:20:05 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header>
-        <a href="/">Marketplace</a>;
+        <Link to="/">Marketplace</Link>;
       </header>
-      <main>products list</main>
-    </>
+      <main>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
