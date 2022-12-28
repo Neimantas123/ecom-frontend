53d15d9e36ce64b04c9eff54b2ce01e22be18851
Neimantas <samochvalovas.neimantas@gmail.com>
2022-12-28 16:30:40 +0200

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Marketplace</Link>;
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
-      </main>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Marketplace</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
