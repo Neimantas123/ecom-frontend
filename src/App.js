0e3526bfc44f1f05980be3ab98228f45e50cc720
Neimantas <samochvalovas.neimantas@gmail.com>
2022-12-28 11:27:39 +0200

    ecom frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <a href="/">Marketplace</a>;
+      </header>
+      <main>products list</main>
+    </>
+  );
+}
+
+export default App;
